383e181bd7b1c1ef08ef94c6fe2533502d0ecd66
YogyerekDEV <mszebi2008@gmail.com>
2026-02-09 20:10:27 +0100

    (MG-01): implemented /users/init endpoint.

--- backend/main.ts
+++ backend/main.ts
@@ -1,11 +1,22 @@
 import express, { Express, Request, Response } from "express";
 import { db } from "./database/db";
+import cookieParser from "cookie-parser";
+import { v4 as uuidv4 } from "uuid";
 const port = 8000;
 
 const app: Express = express();
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Hello world!");
+app.use(cookieParser());
+app.use(express.json());
+
+app.post("/users/init", (req: Request, res: Response) => {
+    let accountID = req.cookies.accountID;
+    if (!accountID) {
+        accountID = uuidv4();
+        res.cookie("accountID", accountID, { httpOnly: true, sameSite: "lax"})
+        return res.json({ accountID, username_set: false });
+    }
+    res.json({ accountID, username_set: false });
 });
 
 app.listen(port, () => {
