25719dfb872f7a2d7380fa6bce092f7304e5d2f2
JackM919 <mdnk2008@gmail.com>
2026-02-14 21:58:00 +0100

    style(MG-02): add style to input in UsernameSetup.tsx

--- frontend/src/components/UsernameSetup.tsx
+++ frontend/src/components/UsernameSetup.tsx
@@ -1,9 +1,11 @@
 export function UsernameSetup() {
-    return(
-        <div>
-            <input type="text" className="border-red-100" />
-            <h1 className="text-red-500">Alma</h1>
+    return (
+        <div className="flex flex-col items-center justify-center p-4">
+            <input 
+                type="text" 
+                className="border-3 border-black rounded-lg p-2 w-full max-w-md focus:outline-none focus:ring-2 focus:to-black focus:border-black transition duration-200 bg-amber-50 text-black" 
+                placeholder="Add username" 
+            />
         </div>
-
     );
 }
