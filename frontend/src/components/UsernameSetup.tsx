ce294b263b2473a8f984561ba8af08eefabb73c4
JackM919 <mdnk2008@gmail.com>
2026-02-14 22:40:35 +0100

    style(MG-02): add text Set Username in UsernameSetup.tsx

--- frontend/src/components/UsernameSetup.tsx
+++ frontend/src/components/UsernameSetup.tsx
@@ -1,15 +1,22 @@
 export function UsernameSetup() {
     return (
-        <div className="items-center justify-center p-4 w-100 h-20 border border-emerald-700 rounded-lg bg-[#333333] scale-100 md:scale-125 lg:scale-150">
-            <div className="flex items-center space-x-1 w-full max-w-md">
-                <input 
-                    type="text" 
-                    className="flex-1 border-3 border-black rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-black focus:border-black transition duration-200 bg-amber-100 text-black" 
-                    placeholder="Add username" 
-                />
-                <button className="bg-black text-white px-4 py-2 rounded-lg hover:bg-gray-800 transition duration-200">
-                    Set
-                </button>
+        <div className="min-h-screen flex items-center justify-center p-4">
+            <div className="scale-100 md:scale-125 lg:scale-150 flex flex-col items-center gap-4">
+                <span className="font-bold text-amber-100 text-4xl">
+                    Set Username...
+                </span>
+                <div className="flex items-center justify-center p-4 border border-emerald-700 rounded-lg bg-[#333333]">
+                    <div className="flex items-center space-x-2 w-full max-w-md">
+                        <input 
+                            type="text"
+                            className="flex-1 border-3 border-black rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-black focus:border-black transition duration-200 bg-amber-100 text-black placeholder-gray-600"
+                            placeholder="Add username"
+                        />
+                        <button className="bg-black text-white px-4 py-2 rounded-lg hover:bg-gray-800 transition duration-200 whitespace-nowrap">
+                            Set
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     );
