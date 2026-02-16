2528592dd5249fbcc452c9ca7e877e9f590189af
D0M1N1Kx <milkovicsd32@gmail.com>
2026-02-16 16:24:38 +0100

    feat(MG-03-01): add GameData class

---
+++ frontend/models/GameData.ts
@@ -0,0 +1,9 @@
+export class GameData {
+    betAmount: number;
+    bombs: number;
+
+    constructor(betAmount: number, bombs: number) {
+        this.betAmount = betAmount;
+        this.bombs = bombs;
+    }
+}
