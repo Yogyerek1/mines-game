0a96f6399d1b4dcb0a0782b279bffc5c6b8585f8
YogyerekDEV <mszebi2008@gmail.com>
2026-02-16 20:29:23 +0100

    feat(MG-03-03): add basic style for Toplist and ProfileData implemented

--- frontend/src/components/Toplist.tsx
+++ frontend/src/components/Toplist.tsx
@@ -1,3 +1,53 @@
+import { ProfileData } from "./profile/ProfileData";
+
 export function Toplist() {
-  return <div>Toplist...</div>;
+  return (
+    <div className="bg-[#181c23] border border-[#23283a] rounded-2xl w-72 py-4 shadow-lg">
+      <h1 className="text-white text-center text-lg font-bold mb-2">
+        LEADERBOARD
+      </h1>
+      <hr className="border-[#23283a] mb-2" />
+      <div className="flex flex-col gap-2 max-h-64 transition-all duration-500 overflow-y-hidden hover:overflow-y-auto">
+        <ProfileData
+          profileURL="https://api.dicebear.com/7.x/avataaars/svg?seed=${uuidv4()}"
+          username="test"
+          score={1500}
+        ></ProfileData>
+        <ProfileData
+          profileURL="https://api.dicebear.com/7.x/avataaars/svg?seed=${uuidv4()}"
+          username="test"
+          score={1500}
+        ></ProfileData>
+        <ProfileData
+          profileURL="https://api.dicebear.com/7.x/avataaars/svg?seed=${uuidv4()}"
+          username="test"
+          score={1500}
+        ></ProfileData>
+        <ProfileData
+          profileURL="https://api.dicebear.com/7.x/avataaars/svg?seed=${uuidv4()}"
+          username="test"
+          score={1500}
+        ></ProfileData>
+        <ProfileData
+          profileURL="https://api.dicebear.com/7.x/avataaars/svg?seed=${uuidv4()}"
+          username="test"
+          score={1500}
+        ></ProfileData>
+        <ProfileData
+          profileURL="https://api.dicebear.com/7.x/avataaars/svg?seed=${uuidv4()}"
+          username="test"
+          score={1500}
+        ></ProfileData>
+      </div>
+      <div className="mt-2 px-4">
+        <div className="h-1 bg-green-400 rounded-full"></div>
+        <div className="text-green-400 text-center mt-1">You</div>
+        <ProfileData
+          profileURL="https://api.dicebear.com/7.x/avataaars/svg?seed=${uuidv4()}"
+          username="test"
+          score={1500}
+        ></ProfileData>
+      </div>
+    </div>
+  );
 }
