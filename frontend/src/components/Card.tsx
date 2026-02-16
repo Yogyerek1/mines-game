f09a3ceb80095d6ea1217f6acca3a2683affe12a
D0M1N1Kx <milkovicsd32@gmail.com>
2026-02-16 22:36:35 +0100

    fix(main): MG-03 merge to main fucked up so i overwrited from here

---
+++ frontend/src/components/Card.tsx
@@ -0,0 +1,12 @@
+import QuestionMark from '../assets/QuestionMark.png'
+import Diamond from '../assets/Diamond.png'
+import Bomb from '../assets/Bomb.png'
+import RedBomb from '../assets/RedBomb.png'
+
+export function Card(){
+    return(
+        <div className="w-18 h-18 bg-gray-800 items-center justify-center border-0 rounded-3xl ">
+            <img src={QuestionMark} alt="" />
+        </div>
+    )
+}
