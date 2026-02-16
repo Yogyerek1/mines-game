36c0a4292b5a7789bb27c5d37dffed86a8f93fad
YogyerekDEV <mszebi2008@gmail.com>
2026-02-16 21:24:41 +0100

    Revert "Merge pull request #8 from Yogyerek1/style/MG-03-03-make-the-toplist-frontend"
    This reverts commit ab1aa7bf9d704a8a6093408c703bf77aedcf8387, reversing
    changes made to d23a21fc713b6a916bda1701f08e7948f74bfacc.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,6 @@ import { ProfileSetup } from "./components/ProfileSetup";
 //import { Button } from "./components/ui-interactive/Button";
 import { type UserData } from "./types/types";
 import { Container } from "./components/Container";
-import { Toplist } from "./components/Toplist";
 
 function App() {
   const [userData, setUserData] = useState<UserData | null>(null);
@@ -105,9 +104,7 @@ function App() {
           */}
           <div className="h-100 w-100 bg-red-500">Game data...</div>
           <div className="h-100 w-100 bg-blue-500">Game...</div>
-          <div className="h-100 w-100">
-            <Toplist myUser={userData} />
-          </div>
+          <div className="h-100 w-100 bg-green-500">Toplist...</div>
         </Container>
       </div>
     );
