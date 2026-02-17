3df2334f6204388a5d97009e78c106447e3aa042
JackM919 <mdnk2008@gmail.com>
2026-02-17 19:34:12 +0100

    style(MG-05): removed username when user is in mobile view

--- frontend/src/components/GameHeader.tsx
+++ frontend/src/components/GameHeader.tsx
@@ -1,20 +1,30 @@
 import { type UserData } from "../types/types"; 
 import { ProfileData } from "./profile/ProfileData";
+import { useState, useEffect } from "react";
 
 type GameHeaderProps = {
     myUser: UserData;
 }
 
 export function GameHeader({ myUser }:GameHeaderProps) {
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+    
+      useEffect(() => {
+          const handleResize = () => setIsMobile(window.innerWidth < 768);
+          window.addEventListener("resize", handleResize);
+          return () => window.removeEventListener("resize", handleResize);
+      }, []);
+
     return (
         <header className="w-full h-30 flex justify-end py-4 sticky top-0 left-0 border-b border-gray-800 items-center">
-            <h1 className="font-bold text-orange-400 ml-16 text-2xl text-left float-left flex-auto ">Mines-Game</h1>
+            <h1 className="font-bold text-orange-400 ml-16 text-2xl text-left float-left flex-auto text-nowrap ">Mines-Game</h1>
         <div className="flex flex-row mr-16">
             <ProfileData
                 profileURL={myUser.profileURL ?? ""}
                 username={myUser.username ?? ""}
                 score={myUser.score ?? 0}
                 scoreEnabled={false}
+                usernameEnabled={!isMobile}
             />
         </div>
         </header>
--- frontend/src/components/profile/ProfileData.tsx
+++ frontend/src/components/profile/ProfileData.tsx
@@ -1,11 +1,13 @@
 import { type UserData } from "../../types/types";
 import { ProfileIMG } from "./ProfileIMG";
 
+
 type ProfileDataProps = {
   scoreEnabled ?: boolean;
+  usernameEnabled ?: boolean;
 } & UserData;
 
-export function ProfileData({ profileURL, username, score, scoreEnabled=true }: ProfileDataProps) {
+export function ProfileData({ profileURL, username, score, scoreEnabled=true, usernameEnabled=true }: ProfileDataProps) {
   return (
     <div className="flex items-center justify-between w-full px-4 py-2 gap-3">
       <ProfileIMG
@@ -15,7 +17,7 @@ export function ProfileData({ profileURL, username, score, scoreEnabled=true }: 
         round="rounded-full"
         border="border border-emerald-600"
       />
-      <span className="flex-1 text-white text-center">{username}</span>
+      {usernameEnabled && <span className="flex-1 text-white text-center">{username}</span>}
       {scoreEnabled && <span className="text-green-400 font-semibold">${score}</span>}
     </div>
   );
