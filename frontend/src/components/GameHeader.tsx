dae4e634fe44f1893e6aff0244bef7a8f587938f
JackM919 <mdnk2008@gmail.com>
2026-02-17 18:29:17 +0100

    style(MG-05): add text to header

---
+++ frontend/src/components/GameHeader.tsx
@@ -0,0 +1,7 @@
+export function GameHeader() {
+    return (
+        <header className="w-full h-30 flex flex-col justify-end py-4 fixed top-0 left-0 border-b border-gray-800">
+            <h1 className="font-bold text-orange-400 text-2xl mt-auto">Mines-React</h1>
+        </header>
+    );
+}
